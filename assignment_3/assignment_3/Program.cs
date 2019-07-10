9119d34e2543810d2a5f8572fb016abf0b24ca60
Heena Heena <735225@student.tor.loc>
2019-07-10 09:18:51 -0400

    second commit

--- assignment_3/assignment_3/Program.cs
+++ assignment_3/assignment_3/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,32 @@ namespace assignment_3
     {
         static void Main(string[] args)
         {
+            var tp = new TextProcessing();
+            tp.Run();
         }
     }
+    class TextProcessing
+    {
+        Queue<string> names = new Queue<string>();
+        public void Run()
+        {
+
+            // Open the names file and access the data
+            using (StreamReader file = new StreamReader("U:/Users/735225/ass_3/assignment_3/assignment_3/TextFile1.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    names.Enqueue(ln);
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+    }
+
 }
